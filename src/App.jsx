34cce81d26fed577b8bfda1747248045c9bc7053
apple <ljt2023@gmail.com>
2026-04-29 16:31:36 +0900

    아자스

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,13 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Routes>
-      {/* 1. 공통 레이아웃을 부모로 설정 */}
       <Route path="/" element={<MainLayout />}>
-        {/* 2. 각 상세 페이지들 */}
         <Route index element={<Home />} />
         <Route path="projects" element={<Projects />} />
         <Route path="projects/:projectId" element={<ProjectDetail />} />
         <Route path="contact" element={<Contact />} />
       </Route>
 
-      {/* 3. 404 페이지 (레이아웃 제외하고 싶을 때 밖에 배치) */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
